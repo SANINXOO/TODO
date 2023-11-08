6a22ad2ac39a7466f9b117509729795d5f0e9f9b
SANINXOO <fuadsaninp@gmail.com>
2023-11-08 23:12:40 +0530

    file added

---
+++ router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import * as controller from "./controller.js";
+
+
+const router=Router();
+router.route("/addTask").post(controller.addTask);
+router.route("/gettask").get(controller.getTask);
+router.route("/deltask/:id").delete(controller.delTask);
+
+export default router;
